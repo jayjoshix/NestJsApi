55def59ab0dc06c1d2ed101ca51eb83aa0eb05a5
Jay <j.joshi128@gmail.com>
2024-07-13 16:24:21 +0530

    changes

---
+++ src/todo/dto/create-todo.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
+
+export class CreateTodoDto {
+    @IsNotEmpty()
+    @IsString()
+    task: string
+
+    @IsOptional()
+    @IsString()
+    description: string  
+
+}
